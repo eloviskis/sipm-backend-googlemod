aca4d8d2438d42925ca7185973548b0ddc890076
eloviskis <eloi.santaroza@gmail.com>
2024-07-22 09:40:42 -0300

    modified:   dist/config/db.js

--- dist/config/db.js
+++ dist/config/db.js
@@ -13,6 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+// Função para conectar ao banco de dados MongoDB
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield mongoose_1.default.connect(process.env.MONGO_URI, {
